c4ddd7d53e901a4600dd4c032c84a72b9634cd73
dpakks <somasundaram.d@northeastern.edu>
2026-07-01 12:42:52 -0400

    Testing Bad PR

--- variables.tf
+++ variables.tf
@@ -3,18 +3,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
-
 variable "instance_name" {
   description = "Name of the EC2 instance"
   type        = string
