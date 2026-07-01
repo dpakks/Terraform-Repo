0e2a7fea09b96d56dfd9bf57ffc70f01af821f87
dpakks <somasundaram.d@northeastern.edu>
2026-07-01 19:21:45 -0400

    Testing Bad PR

--- main.tf
+++ main.tf
@@ -32,20 +32,10 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    ManagedBy = "TerraGuard"
-    Name      = "${var.instance_name}-sg"
-  }
 }
 
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
-  tags = {
-    ManagedBy = "TerraGuard"
-    Name      = var.instance_name
-  }
 }
