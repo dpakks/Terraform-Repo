5ea2e2120e0352020b63df2aff18dc8e42524ec9
dpakks <somasundaram.d@northeastern.edu>
2026-07-01 20:36:38 -0400

    Testing bad commit by human

--- main.tf
+++ main.tf
@@ -37,16 +37,10 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  tags = {
-    ManagedBy = "TerraGuard - built for testing purpose"
-  }
 }
 
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  tags = {
-    ManagedBy = "TerraGuard - built for testing purpose"
-  }
 }
