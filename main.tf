c4db07e038865811147e38d286d3adb5da93e1f3
dpakks <somasundaram.d@northeastern.edu>
2026-07-01 20:47:53 -0400

    Testing good workflow

--- main.tf
+++ main.tf
@@ -42,6 +42,6 @@ resource "aws_instance" "ec2" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   tags = {
-    ManagedBy = "TerraGuard"
+    ManagedBy = "TerraGuard - Built for testing"
   }
 }
